df1d75718b0961cd67346e8c6d00f9c7d49b2324
Selim Ahmed <aronno1920@hotmail.com>
2025-05-21 15:20:35 +0600

    Model Created

---
+++ API/TechTweetAPI/Models/Domain/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace TechTweetAPI.Models.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string UrlHandle { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ API/TechTweetAPI/Models/Domain/Post.cs
@@ -0,0 +1,19 @@
+﻿namespace TechTweetAPI.Models.Domain
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string ShortDesciption { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string UrlHandle { get; set; } = string.Empty;
+        public Boolean IsPublished { get; set; }
+
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime UpdatedDate { get; set; }
+    }
+}
