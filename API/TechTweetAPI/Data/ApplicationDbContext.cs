68e30855fab5d674f0374b715b0bf41d962f7338
Selim Ahmed <aronno1920@hotmail.com>
2025-05-21 15:25:27 +0600

    Create ApplicationDbContext.cs

---
+++ API/TechTweetAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechTweetAPI.Models.Domain;
+
+namespace TechTweetAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public DbSet<Post> Posts { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
