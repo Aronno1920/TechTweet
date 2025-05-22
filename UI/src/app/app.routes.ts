d0ba13b994430efde47c34b254801362951321c1
Selim Ahmed <aronno1920@hotmail.com>
2025-05-22 15:41:50 +0600

    Update Url with RouterLink

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,10 +1,12 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes} from '@angular/router';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { HomeComponent } from './features/public/home/home.component';
+import { CategoryAddComponent } from './features/category/category-add/category-add.component';
 
 export const routes: Routes = [
      {path:'', component:HomeComponent},
-     {path:'admin/categories', component:CategoryListComponent}
+     {path:'admin/categories', component:CategoryListComponent},
+     {path:'admin/categories/add', component:CategoryAddComponent}
 ];
 
 export class AppRoutingModule{}
---
+++ UI/src/app/features/category/category-add/category-add.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-category-add',
+  standalone:true,
+  imports: [RouterLink],
+  templateUrl: './category-add.component.html',
+  styleUrl: './category-add.component.css'
+})
+export class CategoryAddComponent {
+
+}
