df03a0cd2c976ad2a8a07ec689ebd7a23b11d383
Selim Ahmed <aronno1920@hotmail.com>
2025-05-22 15:46:46 +0600

    Import FormsModule

--- UI/src/app/features/category/category-add/category-add.component.ts
+++ UI/src/app/features/category/category-add/category-add.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-category-add',
   standalone:true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './category-add.component.html',
   styleUrl: './category-add.component.css'
 })
+
+
 export class CategoryAddComponent {
 
 }
