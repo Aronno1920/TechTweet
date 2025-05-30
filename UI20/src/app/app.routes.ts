0aa36633c24590277311b75a3aabebc11afedb3d
Selim Ahmed <aronno1920@hotmail.com>
2025-05-30 18:50:20 +0600

    In angular 20, Catagory list is working fine

--- UI20/src/app/app.routes.ts
+++ UI20/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './features/category/category-list/category-list';
+import { Home } from './features/public/home/home';
+import { CategoryAdd } from './features/category/category-add/category-add';
+import { CategoryEdit } from './features/category/category-edit/category-edit';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+     {path:'', component:Home},
+     {path:'admin/categories', component:CategoryList},
+     {path:'admin/categories/add', component:CategoryAdd},
+     {path:'admin/categories/:id', component:CategoryEdit}
+
+];
+
+export class AppRoutingModule{}
